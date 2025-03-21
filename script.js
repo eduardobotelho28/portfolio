e89647065918936aa758d1c1f73c2dc0d8e4ea28
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-03-21 16:52:01 -0300

    responsiveness pt2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let header = document.querySelector('#header')
-
 document.addEventListener('scroll', () => {
 
     if(window.scrollY > 400) header.style.background = '#8b3546'
@@ -8,18 +7,51 @@ document.addEventListener('scroll', () => {
     
 })
 
-let swiper = new Swiper (".mySwiper", {
-    slidesPerView:3,
-    spaceBetween:30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable:true
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl:".swiper-button-prev",
-    },
-});
+function handleWidth () {
+    let getWidth = window.innerWidth || document.documentElement.clientWidth
 
+    let slideShow = 3
 
-  
+    if(getWidth < 1001)
+        slideShow = 2
+    if(getWidth < 701)
+        slideShow = 1
+
+    return slideShow 
+}
+
+function createSwiper (container, pagination, nextButton, prevButton) {
+    return new Swiper (container, {
+        slidesPerView: handleWidth(),
+        spaceBetween:30,
+        pagination: {
+            el: pagination,
+            clickable:true
+        },
+        navigation: {
+            nextEl: nextButton,
+            prevEl: prevButton
+        },
+    });
+}
+
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev")
+
+window.addEventListener('resize', () => {
+    swiper.params.slidesPerView = handleWidth()
+    swiper.update()
+})
+
+const menuHamburguer = document.querySelector('.menu-hamburguer')
+const nav            = document.querySelector('.navBar')
+const links          = document.querySelectorAll('.navbar-links li a')
+
+menuHamburguer.addEventListener('click', ()=> {
+    nav.classList.toggle('active')
+})
+
+links.forEach( link => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('active')
+    })
+})
